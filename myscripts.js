77461230f7677ac351c0d4407981543abe51833d
DeMoravie <32600414+DeMoravie@users.noreply.github.com>
2018-10-31 21:09:45 +0100

    new
    new

---
+++ myscripts.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.left = "0px";
+	document.getElementById("board").style.marginLeft = "200px";
+	document.getElementById("board").style.transition = "0.3s";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.left = "-200px";
+	document.getElementById("board").style.marginLeft = "50px";
+	document.getElementById("board").style.transition = "0.3s";
+}
+
+function openNavR() {
+    document.getElementById("mySidenavR").style.right = "0px";
+}
+
+function closeNavR() {
+    
+	document.getElementById("mySidenavR").style.right = "-200px";
+}
