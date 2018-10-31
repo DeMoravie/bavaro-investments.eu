3bcaf6eee0d9cc562da4b8c4c92e02c1c1743aad
DeMoravie <32600414+DeMoravie@users.noreply.github.com>
2018-10-31 21:35:06 +0100

    new2
    new2

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,14 @@
 function openNav() {
     document.getElementById("mySidenav").style.left = "0px";
 	document.getElementById("board").style.marginLeft = "200px";
+	
 	document.getElementById("board").style.transition = "0.3s";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.left = "-200px";
 	document.getElementById("board").style.marginLeft = "50px";
+	
 	document.getElementById("board").style.transition = "0.3s";
 }
 
